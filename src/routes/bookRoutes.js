23d50c1cffa153829c21e0c1a6b6e8cc23ff9c46
Milan323-debug <milanpramanik2024@gmail.com>
2025-05-21 09:59:11 +0530

    Auth size book  added

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -38,19 +38,21 @@ router.post("/", protectRoute, async (req, res) => {
 //pagination => infinite scroll
 router.get("/", protectRoute, async (req, res) => {
   try {
-    const page = req.query.page || 1;
-    const limit = req.query.limit || 5;
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 5;
     const skip = (page - 1) * limit;
 
-    const books = await Book.find().sort({ createdAt: -1 }).skip(skip).limit(limit).populate("user", "username profileImage");
+    const books = await Book.find().sort({ createdAt: -1 })
+      .skip(skip)
+      .limit(limit)
+      .populate("user", "username profileImage");
 
     const total = await Book.countDocuments();
-    res.send({
-        books,
-        currentPage: page,
-        totalBooks,
-        totalPages: Math.ceil(total / limit),
-
+    res.json({
+      books,
+      currentPage: page,
+      totalBooks: total,
+      totalPages: Math.ceil(total / limit),
     });
   } catch (err) {
     console.error("Error fetching books:", err);
