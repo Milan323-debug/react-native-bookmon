b785addf2b48db400123db0451eccf66aa436930
Milan323-debug <milanpramanik2024@gmail.com>
2025-05-25 11:14:14 +0530

    Auth new  added

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -71,6 +71,7 @@ router.post('/register', async (req, res) => {
                 username: user.username,
                 email: user.email,
                 profileImage: user.profileImage,
+                createdAt: user.createdAt
             },
         });
     } catch (error) {
@@ -106,6 +107,7 @@ router.post('/login', async (req, res) => {
                 username: user.username,
                 email: user.email,
                 profileImage: user.profileImage,
+                createdAt: user.createdAt
             },
         })
     } catch (error) {
